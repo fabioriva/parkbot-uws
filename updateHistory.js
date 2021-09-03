5ebd8a91fd06e05af428bb20f6c04ed6d80d3d60
fabioriva <f.riva@sotefin.ch>
2021-09-03 14:20:12 +0200

    mongodb wallstreet history update

--- updateHistory.js
+++ updateHistory.js
@@ -17,9 +17,14 @@ const start = async () => {
     await client.connect()
     const db = client.db(DATABASE)
 
-    // updateAlarms(db, 1, 1, obj)
     for (let i = 1; i <= 64; i++) {
       updateAlarms(db, 1, i, obj)
+    }
+    for (let i = 0; i <= 15; i++) {
+      updateOperations(db, i, obj)
+    }
+    for (let i = 0; i <= 8; i++) {
+      updateModes(db, i, obj)
     }
   } catch (err) {
     console.error(new Error(err))
@@ -49,6 +54,50 @@ const updateAlarms = util.promisify(async (db, device, alarm, obj) => {
   const updateDoc = {
     $set: {
       alarm: obj.alarms[device - 1].alarms[alarm - 1]
+    }
+  }
+  const result = await db.collection(COLLECTION).updateMany(filter, updateDoc)
+  console.log(result)
+  // return result
+})
+
+const updateOperations = util.promisify(async (db, id, obj) => {
+  const filter = {
+    'operation.id': id
+  }
+  console.log(filter)
+  const cursor = await db.collection(COLLECTION).find(filter)
+  // print a message if no documents were found
+  if ((await cursor.count()) === 0) {
+    console.log('No documents found!')
+  }
+  // replace console.dir with your callback to access individual elements
+  await cursor.forEach(console.dir)
+  const updateDoc = {
+    $set: {
+      operation: obj.operations[id]
+    }
+  }
+  const result = await db.collection(COLLECTION).updateMany(filter, updateDoc)
+  console.log(result)
+  // return result
+})
+
+const updateModes = util.promisify(async (db, id, obj) => {
+  const filter = {
+    'mode.id': id
+  }
+  console.log(filter)
+  const cursor = await db.collection(COLLECTION).find(filter)
+  // print a message if no documents were found
+  if ((await cursor.count()) === 0) {
+    console.log('No documents found!')
+  }
+  // replace console.dir with your callback to access individual elements
+  await cursor.forEach(console.dir)
+  const updateDoc = {
+    $set: {
+      mode: obj.modes[id]
     }
   }
   const result = await db.collection(COLLECTION).updateMany(filter, updateDoc)
