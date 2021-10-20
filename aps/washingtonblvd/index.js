2c909163d53b163588aa136f5e64238bb3d81331
fabioriva <f.riva@sotefin.ch>
2021-10-20 10:05:29 +0200

    remove aps temp routes

--- aps/washingtonblvd/index.js
+++ aps/washingtonblvd/index.js
@@ -9,15 +9,6 @@ const routes = require('../../lib/routes')
 const websocket = require('../../lib/websocket')
 const parkworks = require('./parkworksApi') // Parkworks API
 
-const format = require('date-fns/format')
-const querystring = require('querystring')
-const {
-  getHistory_,
-  getOperations_,
-  getRecentActivity_
-} = require('../../lib/history')
-const { sendJson } = require('../../lib/json')
-
 const prefix = '/aps/washingtonblvd'
 
 const client = new MongoClient(process.env.MONGODB_URI, {
@@ -36,42 +27,6 @@ const start = async () => {
         console.log('Failed to listen to port ' + def.HTTP)
       }
     })
-
-    app.get(prefix + '/dashboard', async (res, req) => {
-      res.onAborted(() => {
-        res.aborted = true
-      })
-      const activity = await getRecentActivity_(db, 6)
-      const statistics = await getOperations_(db, {
-        dateString: format(new Date(), 'yyyy-MM-dd')
-      })
-      sendJson(res, {
-        activity: activity,
-        // cards: obj.cards.length,
-        occupancy: obj.map.occupancy,
-        operations: statistics,
-        system: obj.overview
-      })
-    })
-
-    app.get(prefix + '/history', async (res, req) => {
-      res.onAborted(() => {
-        res.aborted = true
-      })
-      const query = querystring.parse(req.getQuery())
-      const docs = await getHistory_(db, query)
-      sendJson(res, docs)
-    })
-
-    app.get(prefix + '/statistics', async (res, req) => {
-      res.onAborted(() => {
-        res.aborted = true
-      })
-      const query = querystring.parse(req.getQuery())
-      const docs = await getOperations_(db, query)
-      sendJson(res, docs)
-    })
-
     const plc01 = new PLC(def.PLC)
     plc01.main(def, obj)
     plc01.on('pub', ({ channel, data }) => app.publish(channel, data))
